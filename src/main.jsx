02213a53dbe2265138f48bed2846a3242e2f9fce
Kasansa Kuya <Mr.Kuya@outlook.com>
2024-11-16 14:49:22 +0200

    4th commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Dashboard from './routes/Dashboard.jsx'
-import Contact from './routes/Contact.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard/>,
-  },
-  {
-    path: '/contact',
-    element: <Contact/>,
-  },
-])
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Dashboard from './routes/Dashboard';
+import Contact from './routes/Contact';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
